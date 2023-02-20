6a4fc97fc4244eda5b73cd6010088b3c008a713c
吴桐 <wutong@gzsonic.com>
2023-02-20 17:10:42 +0800

    ༼ つ ◕_◕ ༽つ

--- backend/app/app/api/api_v1/router/home/index.py
+++ backend/app/app/api/api_v1/router/home/index.py
@@ -5,16 +5,13 @@ from app.api.utils.responseCode import resp_200
 router = APIRouter()
 
 
-async def action(
+def todos(
         limit: int = 8,
         page: int = 1,
-        username: str = None,
-        module_name: str = None,
         start_time: str = None,
         end_time: str = None
 ):
     return resp_200()
 
 
-router.add_api_route(methods=['GET'], path="/",
-                     endpoint=action, summary="action")
+router.add_api_route(methods=['GET'], path="/", endpoint=todos, summary="todos")
--- backend/app/app/db/mongo_db.py
+++ backend/app/app/db/mongo_db.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*
+from typing import Generator
+
 from app.config import settings
 
 from pymongo import MongoClient
@@ -17,3 +19,16 @@ async def get_mongo_database() -> MongoClient:
 
 mongo_c_ = MongoClient(host=settings.MONGO_HOST, port=settings.MONGO_PORT, username=settings.MONGO_USER,
                  password=settings.MONGO_PASS)
+
+
+def get_db() -> Generator:
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        try:
+            db.commit()
+        except exc.SQLAlchemyError:
+            db.rollback()
+            print("ERROR DB COMMIT")
+        db.close()
